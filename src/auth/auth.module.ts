54e94bf8d38e3a262a657fbb1e0abd13d674b294
Forrest Walker <me@forrestwalker.net>
2020-05-10 15:01:04 -0500

    Connected UserRepository to auth module, init passport module and jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from '../user/user.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+    imports: [
+        ConfigModule,
+        TypeOrmModule.forFeature([UserRepository]),
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                secret: configService.get<string>('jwt.secret'),
+                signOptions: {
+                    expiresIn: configService.get<number>(
+                        'jwt.signOptions.expiresIn',
+                    ),
+                },
+            }),
+            inject: [ConfigService],
+        }),
+    ],
     providers: [AuthService],
 })
 export class AuthModule {}
