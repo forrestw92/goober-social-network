1f6d67fd54979b958052239bdb308c7ad7be5c66
Forrest Walker <me@forrestwalker.net>
2020-05-10 13:50:32 -0500

    Init nestjs config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import databaseConfig from './config/database.config';
+import jwtConfig from './config/jwt.config';
 
 @Module({
-  imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: ['.env.development', '.env.production'],
+            load: [databaseConfig, jwtConfig],
+            isGlobal: true,
+        }),
+    ],
 })
 export class AppModule {}
