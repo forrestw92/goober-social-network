16d29f9a431b58e8286a11eea8843ee61b3ee966
Forrest Walker <me@forrestwalker.net>
2020-05-10 13:57:50 -0500

    Added create user input

---
+++ src/user/user.input.ts
@@ -0,0 +1,57 @@
+import { Field, InputType } from '@nestjs/graphql';
+import {
+    IsDateString,
+    IsEmail,
+    IsString,
+    Matches,
+    MaxLength,
+    MinLength,
+} from 'class-validator';
+
+@InputType()
+export class CreateUserInput {
+    @Field()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(100)
+    firstName: string;
+
+    @Field()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(100)
+    lastName: string;
+
+    @Field()
+    @IsEmail()
+    email: string;
+
+    @Field()
+    @IsString()
+    @MinLength(2)
+    @MaxLength(25)
+    username: string;
+
+    @Field()
+    @IsString()
+    @IsDateString()
+    birthDate: string;
+
+    @Field()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(50)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'Password to weak',
+    })
+    password: string;
+
+    @Field()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(50)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'Password to weak',
+    })
+    confirmPassword: string;
+}
