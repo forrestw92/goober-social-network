0a43698ccd0b0af6a52faf64362c7b3d74d16ac0
Forrest Walker <me@forrestwalker.net>
2020-05-10 15:10:50 -0500

    Added auth guard to protect routes and auth service to generate and validate access token(bearer tokens)

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,42 @@
+import {
+    BadRequestException,
+    ExecutionContext,
+    Injectable,
+    UnauthorizedException,
+} from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+    constructor(private readonly jwtService: AuthService) {
+        super();
+    }
+    getRequest(context: ExecutionContext) {
+        const ctx = GqlExecutionContext.create(context);
+        return ctx.getContext().req;
+    }
+    canActivate(context: ExecutionContext) {
+        const authHeader = GqlExecutionContext.create(context)
+            .getContext()
+            .req.header('Authorization');
+        if (!authHeader) {
+            throw new BadRequestException('Authorization header not found.');
+        }
+
+        const [type, token] = authHeader.split(' ');
+        if (type !== 'Bearer') {
+            throw new BadRequestException(
+                `Authentication type \'Bearer\' required. Found \'${type}\'`,
+            );
+        }
+        const validationResult = this.jwtService.validateAccessToken(token);
+
+        if (validationResult === true) {
+            return super.canActivate(context);
+        }
+
+        throw new UnauthorizedException(validationResult);
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../user/user.entity';
+import { JwtPayload } from './jwt.payload';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly jwtService: JwtService) {}
+
+    async createAccessToken(user: User): Promise<{ accessToken: string }> {
+        const { username } = user;
+        const payload: JwtPayload = { username };
+        const accessToken = await this.jwtService.sign(payload);
+        return { accessToken };
+    }
+
+    validateAccessToken(token: string): boolean {
+        try {
+            this.jwtService.verify(token);
+            return true;
+        } catch (error) {
+            return error.name;
+        }
+    }
+}
