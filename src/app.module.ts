ba123dbfa8e6d4f4ab726edc3cce74c66ffea3af
Forrest Walker <me@forrestwalker.net>
2020-05-10 14:03:28 -0500

    Init TypeOrm Module and GraphQL Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 
 import databaseConfig from './config/database.config';
 import jwtConfig from './config/jwt.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
     imports: [
@@ -11,6 +13,24 @@ import jwtConfig from './config/jwt.config';
             envFilePath: ['.env.development', '.env.production'],
             load: [databaseConfig, jwtConfig],
             isGlobal: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mongodb',
+                username: configService.get<string>('username'),
+                port: configService.get<number>('port'),
+                password: configService.get<string>('password'),
+                host: configService.get<string>('host'),
+                database: configService.get<string>('name'),
+                synchronize: configService.get<boolean>('synchronize'),
+                entities: [__dirname + '/**/*.entity.{js,ts}'],
+                useUnifiedTopology: true,
+            }),
+            inject: [ConfigService],
+        }),
+        GraphQLModule.forRoot({
+            autoSchemaFile: true,
         }),
         UserModule,
     ],
