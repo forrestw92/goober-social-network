8050f29db0d0d43c28d59f9e2ef9b8d6411bd479
Forrest Walker <me@forrestwalker.net>
2020-05-10 13:54:56 -0500

    Added user type

---
+++ src/user/user.type.ts
@@ -0,0 +1,34 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+
+@ObjectType('User')
+export class UserType {
+    @Field()
+    id: string;
+
+    @Field()
+    firstName: string;
+
+    @Field()
+    lastName: string;
+
+    @Field()
+    birthDate: string;
+
+    @Field()
+    email: string;
+
+    @Field()
+    username: string;
+
+    @Field()
+    password: string;
+
+    @Field()
+    salt: string;
+
+    @Field()
+    createdAt: string;
+
+    @Field()
+    updatedAt: string;
+}
